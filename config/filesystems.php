62479b3ab057ad97a3017d67f5f81f4a796837b7
Nikita V <swebs.sh@gmail.com>
2025-10-10 11:48:24 +0500

    Установил moonshine

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,11 +40,10 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('/'),
+            'url' => env('APP_URL') . '/',
             'visibility' => 'public',
             'throw' => false,
-            'report' => false,
         ],
 
         's3' => [
