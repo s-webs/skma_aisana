5e0c9ca93b9be3b3ee4ece6cc462aff567278d60
Nikita V <swebs.sh@gmail.com>
2025-11-11 14:33:51 +0500

    Переработал дизайн добавил модели и контроллеры

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\Public\HomeController::class, 'index'])->name('home.index');
 
-Route::get('projects', [\App\Http\Controllers\Public\ProjectsController::class, 'index'])->name('projects.index');
-Route::get('projects/{slug}', [\App\Http\Controllers\Public\ProjectsController::class, 'show'])->name('projects.show');
+Route::get('ai-agents', [\App\Http\Controllers\Public\ProjectsController::class, 'index'])->name('projects.index');
+Route::get('ai-agents/{slug}', [\App\Http\Controllers\Public\ProjectsController::class, 'show'])->name('projects.show');
 
 Route::get('news', [\App\Http\Controllers\Public\NewsController::class, 'index'])->name('news.index');
 Route::get('news/{slug}', [\App\Http\Controllers\Public\NewsController::class, 'show'])->name('news.show');
