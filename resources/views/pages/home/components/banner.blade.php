3c5efa8df4925aa1eb0b49c0a16ee45378bcc5d3
Nikita V <swebs.sh@gmail.com>
2025-11-11 14:39:22 +0500

    Переработал дизайн добавил модели и контроллеры

--- resources/views/pages/home/components/banner.blade.php
+++ resources/views/pages/home/components/banner.blade.php
@@ -1,7 +1,8 @@
 <div class="h-[560px] w-full relative">
     <div class="h-full">
         <div class="z-[10] absolute w-full h-full bottom-0 left-0">
-            <img src="/assets/img/pattern_05.png" alt="" class="w-full h-full lg:object-cover object-cover lg:object-top">
+            <img src="/assets/img/pattern_05.webp" alt=""
+                 class="w-full h-full lg:object-cover object-cover lg:object-top">
         </div>
         <div class="container mx-auto h-full relative z-[20]">
             <div class="h-full">
@@ -9,9 +10,9 @@
                     <div class="text-center text-[48px] font-bold">
                         <h1 class="text-custom-main">AI-SANA</h1>
                         <h2 class="text-custom-mainDark lg:text-custom-secondary">Трамплин технологий</h2>
-{{--                        <div class="mt-[50px]">--}}
-{{--                            <a href="#" class="text-[24px] font-medium border-2 border-custom-main bg-white hover:bg-custom-main text-custom-main hover:text-custom-primary transition-colors duration-300 rounded-[12px] px-[50px] py-[12px]">Презентация</a>--}}
-{{--                        </div>--}}
+                        {{--                        <div class="mt-[50px]">--}}
+                        {{--                            <a href="#" class="text-[24px] font-medium border-2 border-custom-main bg-white hover:bg-custom-main text-custom-main hover:text-custom-primary transition-colors duration-300 rounded-[12px] px-[50px] py-[12px]">Презентация</a>--}}
+                        {{--                        </div>--}}
                     </div>
                 </div>
             </div>
